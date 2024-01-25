4a72e31048613cb041afe3e5051a40f31e732846
Lino09 <onosmx@gmail.com>
2024-01-24 20:58:05 -0600

    fix: attempt to prevent enter key from actioning last clicked button

--- components/VirtualKeyboard.tsx
+++ components/VirtualKeyboard.tsx
@@ -4,13 +4,22 @@ const keys = [
   ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"],
 ];
 
-const VirtualKeyboard = ({ onClickChar }: any) => {
+const VirtualKeyboard = ({ onClickChar }: { onClickChar: Function }) => {
+  const handleClicker = (e: any) => {
+    e.preventDefault();
+    if (e.pageX === 0 && e.pageY === 0) onClickChar({ key: "Enter" });
+    else if (e.target.innerText === "Delete") onClickChar({ key: "Backspace" });
+    else {
+      onClickChar({ key: e.target.innerText });
+    }
+  };
   return (
     <div className=" max-w-3xl rounded-xl w-full mt-8 bg-gray-200 p-8 flex flex-col font-bold text-2xl gap-2 dark:bg-[#DADCE0] dark:bg-opacity-5">
       <div className="flex gap-2 ml-8">
         {keys[0].map((key) => (
           <button
-            onClick={() => onClickChar({ key })}
+            tabIndex={-1}
+            onClick={(e) => handleClicker(e)}
             className="w-14 h-16 bg-gray-300 dark:bg-[#565F7E] dark:hover:bg-opacity-50 hover:bg-opacity-50 rounded-lg shadow"
             key={key}
           >
@@ -21,7 +30,8 @@ const VirtualKeyboard = ({ onClickChar }: any) => {
       <div className="flex gap-2 ml-12">
         {keys[1].map((key) => (
           <button
-            onClick={() => onClickChar({ key })}
+            tabIndex={-1}
+            onClick={(e) => handleClicker(e)}
             className="w-14 h-16 bg-gray-300 dark:bg-[#565F7E] dark:hover:bg-opacity-50 hover:bg-opacity-50 rounded-lg shadow"
             key={key}
           >
@@ -32,11 +42,8 @@ const VirtualKeyboard = ({ onClickChar }: any) => {
       <div className="flex gap-2">
         {keys[2].map((key) => (
           <button
-            onClick={
-              key === "Delete"
-                ? () => onClickChar({ key: "Backspace" })
-                : () => onClickChar({ key })
-            }
+            tabIndex={-1}
+            onClick={(e) => handleClicker(e)}
             className="min-w-14 h-16 px-2 bg-gray-300 dark:hover:bg-opacity-50 hover:bg-opacity-50 dark:bg-[#565F7E] rounded-lg shadow"
             key={key}
           >
